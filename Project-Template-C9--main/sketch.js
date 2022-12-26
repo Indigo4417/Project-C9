8d11e9fdbb4885f0b4b480ae0bcf1f3eec755a3e
Indigo4417 <121071168+Indigo4417@users.noreply.github.com>
2022-12-26 10:50:50 -0500

    Add files via upload

---
+++ Project-Template-C9--main/sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+ background(30);
+ background("blue");
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    background("red");
+  }
+
+  
+  drawSprites();
+}
+
